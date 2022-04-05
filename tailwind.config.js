98f6ce3c4290803b8347e28d936b23038c9b9331
noel2017mdala <noelmdala2017@gmail.com>
2022-04-05 18:33:11 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      sm: "60px",
+      // => @media (min-width: 576px) { ... }
+
+      smd: "640px",
+
+      md: "950px",
+      // => @media (min-width: 960px) { ... }
+
+      lg: "1440px",
+      // => @media (min-width: 1440px) { ... }
+    },
+    extend: {
+      colors: {
+        primary: "#2C2C2C",
+        secondary: "#FFB533",
+        primary2: "#313131",
+      },
+    },
+  },
+  plugins: [],
+};
